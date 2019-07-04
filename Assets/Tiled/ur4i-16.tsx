9010d337aa2ffb48ead6615da493b7777e2f6679
Brandon Cordell Jones <bcjones1@fullsail.edu>
2019-07-04 19:57:49 -0400

    # If applied, this commit will... Remove duplicate tiled files Add final initial tiled files # Why is this change needed? Prior to this change,
    # How does it address the issue?
    This change
    
    # Provide links to any relevant tickets, articles or other resources

--- Assets/Tiled/ur4i-16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.06.20" name="ur4i-16" tilewidth="16" tileheight="16" tilecount="448" columns="16">
- <image source="../../../../Resources/Sprites/2d-metroidvania-tileset-16x16 3/s4m_ur4i-metroidvania-1.3-high-contrast.png" width="256" height="448"/>
-</tileset>
--- Assets/Tiled/ur4i.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.06.20" name="ur4i" tilewidth="32" tileheight="32" tilecount="112" columns="8">
- <grid orientation="orthogonal" width="16" height="16"/>
- <image source="../../../../Resources/Sprites/2d-metroidvania-tileset-16x16 3/s4m_ur4i-metroidvania-1.3-high-contrast.png" width="256" height="448"/>
-</tileset>
